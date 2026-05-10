e10b5889bb301545f6a1acf8516e626a1aae6935
GaaloP <gposada24@alumnos.uaq.mx>
2026-05-10 17:17:02 -0600

    feat(users): add user module and service

--- back-ai-ticket-sys/src/users/users.service.ts
+++ back-ai-ticket-sys/src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findByUsername(username: string) {
+    return this.prisma.user.findUnique({
+      where: { username },
+    });
+  }
+
+  async findByUuid(uuid: string) {
+    return this.prisma.user.findUnique({
+      where: { uuid },
+    });
+  }
+}
